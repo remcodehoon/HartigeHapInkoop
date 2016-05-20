07e08dd02041d0033523bdb8e4e3d30d05d4c068
Remco de Hoon <remcodehoon@hotmail.com>
2016-05-20 11:50:39 +0200

    DatabaseConnection class optimalization

--- src/main/java/datastore/DatabaseConnection.java
+++ src/main/java/datastore/DatabaseConnection.java
@@ -5,154 +5,125 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseConnection {
-    
+
     private Connection connection;
     private Statement statement;
-    
-    public DatabaseConnection()
-    {
+    private static final Logger log = Logger.getLogger(DatabaseConnection.class.getName());
+
+    public DatabaseConnection() {
         connection = null;
         statement = null;
     }
-    
-    public boolean openConnection()
-    {
+
+    public boolean openConnection() {
         boolean result = false;
-        if(connection == null)
-        {
+        if (connection == null) {
             Properties prop = new Properties();
             InputStream input = null;
-        
-            try
-            {   
+
+            try {
                 input = new FileInputStream("config.properties");
 
-		// load a properties file
-		prop.load(input);
-                
-                connection = DriverManager.getConnection(
-                    "jdbc:mysql://" + prop.getProperty("ip") + "/" + prop.getProperty("dbname") , prop.getProperty("dbuser"), prop.getProperty("dbpassword"));
+                // load a properties file
+                prop.load(input);
 
-                if(connection != null)
-                {
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://" + prop.getProperty("ip") + "/" + prop.getProperty("dbname"), prop.getProperty("dbuser"), prop.getProperty("dbpassword"));
+
+                if (connection != null) {
                     statement = connection.createStatement();
+                } else {
+                    result = true;
                 }
-                else
-                
-                result = true;
-            }
-            catch(SQLException e)
-            {
-                System.out.println(e);
+            } catch (SQLException | IOException e) {
+                log.log( Level.SEVERE, e.toString(), e);
                 result = false;
+            } finally {
+                if (input != null) {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        log.log( Level.SEVERE, e.toString(), e);
+                        result = false;
+                    }
+                }
             }
-            catch (IOException ex) {
-		ex.printStackTrace();
-            } 
-            finally {
-		if (input != null) {
-			try {
-				input.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-            }
-        }
-        else
-        {
+        } else {
             // A connection was already initalized.
             result = true;
         }
-        
+
         return result;
     }
-    
-    public boolean connectionIsOpen()
-    {
+
+    public boolean connectionIsOpen() {
         boolean open = false;
-        
-        if(connection != null && statement != null)
-        {
-            try
-            {
+
+        if (connection != null && statement != null) {
+            try {
                 open = !connection.isClosed() && !statement.isClosed();
-            }
-            catch(SQLException e)
-            {
+            } catch (SQLException e) {
                 System.out.println(e);
                 open = false;
             }
         }
         // Else, at least one the connection or statement fields is null, so
         // no valid connection.
-        
+
         return open;
     }
-    
-    public void closeConnection()
-    {
-        try
-        {
+
+    public void closeConnection() {
+        try {
             statement.close();
             connection.close();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
-    public ResultSet executeSQLSelectStatement(String query)
-    {
+
+    public ResultSet executeSQLSelectStatement(String query) {
         ResultSet resultset = null;
-        
+
         // First, check whether a some query was passed and the connection with
         // the database.
-        if(query != null && connectionIsOpen())
-        {
+        if (query != null && connectionIsOpen()) {
             // Then, if succeeded, execute the query.
-            try
-            {
+            try {
                 resultset = statement.executeQuery(query);
-            }
-            catch(SQLException e)
-            {
+            } catch (SQLException e) {
                 System.out.println(e);
                 resultset = null;
             }
         }
-        
+
         return resultset;
     }
-    
-    public boolean executeSQLDeleteStatement(String query)
-    {
+
+    public boolean executeSQLDeleteStatement(String query) {
         boolean result = false;
-        
+
         // First, check whether a some query was passed and the connection with
         // the database.
-        if(query != null && connectionIsOpen())
-        {
+        if (query != null && connectionIsOpen()) {
             // Then, if succeeded, execute the query.
-            try
-            {
+            try {
                 statement.executeUpdate(query);
                 result = true;
-            }
-            catch(SQLException e)
-            {
+            } catch (SQLException e) {
                 System.out.println(e);
                 result = false;
             }
         }
-        
+
         return result;
     }
-    
-    public boolean executeSQLInsertStatement(String query)
-    {
+
+    public boolean executeSQLInsertStatement(String query) {
         // First, check whether a some query was passed and the connection
         // with the database.
         boolean gelukt = false;
@@ -167,4 +138,3 @@ public class DatabaseConnection {
         return gelukt;
     }
 }
-
